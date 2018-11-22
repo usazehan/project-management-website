03ec0e660fb8d62f8b2dd02d4b51ec9a02893a60
Zehan Sunesara <usazehan@gmail.com>
2018-11-21 18:03:34 -0600

    Created operations

--- trelloclone/src/main/java/io/zehansunesara/trelloclone/domain/ProjectTask.java
+++ trelloclone/src/main/java/io/zehansunesara/trelloclone/domain/ProjectTask.java
@@ -3,6 +3,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 public class ProjectTask {
@@ -10,6 +11,7 @@ public class ProjectTask {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Summary cannot be blank")
     private  String summary;
     private String acceptanceCriteria;
     private String status;
--- trelloclone/src/main/java/io/zehansunesara/trelloclone/service/ProjectTaskService.java
+++ trelloclone/src/main/java/io/zehansunesara/trelloclone/service/ProjectTaskService.java
@@ -1,5 +1,6 @@
 package io.zehansunesara.trelloclone.service;
 
+import io.zehansunesara.trelloclone.domain.ProjectTask;
 import io.zehansunesara.trelloclone.repository.ProjectTaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,4 +10,10 @@ public class ProjectTaskService {
 
     @Autowired
     private ProjectTaskRepository projectTaskRepository;
+    public ProjectTask saveOrUpdateProjectTask(ProjectTask projectTask) {
+        if (projectTask.getStatus() == null || projectTask.getStatus() == "") {
+            projectTask.setStatus("TO_DO");
+        }
+        return projectTaskRepository.save(projectTask);
+    }
 }
