6e135f0c452ddac933cf59944bced94c8e818bcd
Zehan Sunesara <usazehan@gmail.com>
2018-11-27 15:21:09 -0600

    Setup dependencies

---
+++ trello-clone-react/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1 className="alert alert-warning">Welcome to Trello</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
