8bdee36dca6967a7d6d2850d3f02b161cfc6ffa1
Zehan Sunesara <usazehan@gmail.com>
2018-11-29 16:34:49 -0600

    Setup routing

--- trello-clone-react/src/App.js
+++ trello-clone-react/src/App.js
@@ -3,14 +3,19 @@ import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from './components/NavBar';
 import TrelloClone from './components/TrelloClone';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import AddProjectTask from './components/ProjectTask/AddProjectTask';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <NavBar />
-        <TrelloClone />
-      </div>
+      <Router>
+        <div className="App">
+          <NavBar />
+          <Route exact path="/" component={TrelloClone}/>
+          <Route exact path="/addProjectTask" component={AddProjectTask}/>
+        </div>
+      </Router>
     );
   }
 }
