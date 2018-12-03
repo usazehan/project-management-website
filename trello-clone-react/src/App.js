4228fdb12706b696a5d606615b22faaafb7835fd
Zehan Sunesara <usazehan@gmail.com>
2018-12-03 17:25:12 -0600

    Created redux store

--- trello-clone-react/src/App.js
+++ trello-clone-react/src/App.js
@@ -5,17 +5,21 @@ import NavBar from './components/NavBar';
 import TrelloClone from './components/TrelloClone';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import AddProjectTask from './components/ProjectTask/AddProjectTask';
+import {Provider} from 'react-redux';
+import store from './store';
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <NavBar />
-          <Route exact path="/" component={TrelloClone}/>
-          <Route exact path="/addProjectTask" component={AddProjectTask}/>
-        </div>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <NavBar />
+            <Route exact path="/" component={TrelloClone}/>
+            <Route exact path="/addProjectTask" component={AddProjectTask}/>
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
