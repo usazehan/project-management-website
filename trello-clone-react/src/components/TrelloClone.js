ae0f54bfda418abc877d47070a01c4fd66582464
Zehan Sunesara <usazehan@gmail.com>
2018-12-07 22:47:33 -0600

    Display project tasks

--- trello-clone-react/src/components/TrelloClone.js
+++ trello-clone-react/src/components/TrelloClone.js
@@ -10,6 +10,36 @@ class TrelloClone extends Component {
     this.props.getBacklog();
   }
   render() {
+    const {project_tasks} = this.props.project_tasks;
+    let Content;
+    let toDoItems = [];
+    let inProgressItems = [];
+    let doneItems = [];
+    const projectAlgorithm = project_tasks => {
+      if(project_tasks.length < 1) {
+        return (
+          <div className="alert alert-info text-center" role="alert">
+            No project tasks on this board 
+          </div>
+        )
+      } else {
+        const tasks = project_tasks.map(project_task => (
+          <ProjectTaskItem key={project_task.id} project_task={project_task}/>
+        ));
+        for(let i = 0; i< tasks.length; i++) {
+          if (tasks[i].props.project_task.status === "TO_DO") {
+            toDoItems.push(tasks[i]);
+          }
+          if (tasks[i].props.project_task.status === "IN_PROGRESS") {
+            inProgressItems.push(tasks[i]);
+          }
+          if (tasks[i].props.project_task.status === "DONE") {
+            doneItems.push(tasks[i]);
+          }
+        }
+      }
+    };
+    projectAlgorithm(project_tasks);
     return (
       <div className="container">
         <Link to="/addProjectTask" className="btn btn-primary mb-3">
@@ -28,10 +58,11 @@ class TrelloClone extends Component {
               {
                 // <!-- SAMPLE PROJECT TASK STARTS HERE -->
               }{" "}
-              <ProjectTaskItem />
+              
               {
                 // <!-- SAMPLE PROJECT TASK ENDS HERE -->
               }{" "}
+              {toDoItems}
             </div>
             <div className="col-md-4">
               <div className="card text-center mb-2">
@@ -43,7 +74,7 @@ class TrelloClone extends Component {
                 //     <!-- SAMPLE PROJECT TASK STARTS HERE -->
                 // <!-- SAMPLE PROJECT TASK ENDS HERE -->
               }
-              <ProjectTaskItem />
+              {inProgressItems}
             </div>
             <div className="col-md-4">
               <div className="card text-center mb-2">
@@ -61,6 +92,7 @@ class TrelloClone extends Component {
         {
           //<!-- Backlog ENDS HERE -->
         }{" "}
+        {doneItems}
       </div>
     );
   }
